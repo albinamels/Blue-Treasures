7b89b36e5f8aa2f238469a357faeb6817f1f47ee
albinamels <albina.mels@gmail.com>
2023-10-10 23:10:54 -0500

    init commit

---
+++ src/components/Services.js
@@ -0,0 +1,31 @@
+import { services } from "../data";
+import Title from "./Title";
+import Service from "./Service";
+
+const Services = () => {
+  return (
+    <section className="section services" id="services">
+      <Title title="our" subTitle="services" />
+
+      <div className="section-center services-center">
+        {services.map((service) => {
+          return <Service key={service.id} {...service} />;
+          // return (
+
+          // <article className="service" key={id}>
+          //   <span className="service-icon">
+          //     <i className={icon}></i>
+          //   </span>
+          //   <div className="service-info">
+          //     <h4 className="service-title">{title}</h4>
+          //     <p className="service-text">{text}</p>
+          //   </div>
+          // </article>
+          // );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
